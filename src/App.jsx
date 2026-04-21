51fa96df10d4061c00bf63f38b89b26a9e1cbd01
Manas Sandhu <manassandhu@Manass-MacBook-Pro.local>
2026-04-21 18:58:52 +0530

    Add full React app code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 
-// import Play from "./Play";
+import Play from "./Play";
 import Home from "./Home";
 
 function App() {
   return (
     <div className="app">
       <Home/>
-      {/* <Play/> */}
+      <Play/>
     </div>
   );
 }
