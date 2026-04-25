137445852fa3c81ddbbf378bc0e122202287548e
Manas Sandhu <manassandhu@Manass-MacBook-Pro.local>
2026-04-25 14:27:33 +0530

    Fixing path bugs for login signup

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
-
 import Play from "./Play";
 import Home from "./Home";
 import Layout from "./Layout";
 import AboutFAQ from "./components/AboutFAQ/AboutFAQ";
-
-import Logincard from "/Users/palakpreetkaurbhullar/Documents/Playo-Clone-using-React/loginSignup/loginSignup components/Logincard.jsx";
-import Signupcard from "/Users/palakpreetkaurbhullar/Documents/Playo-Clone-using-React/loginSignup/loginSignup components/Signupcard.jsx";
+import Logincard from "./Login_signup/Logincard"
+import Signupcard from "./Login_signup/Signupcard"
 
 function App() {
   const [toggleAuth, setToggleAuth] = useState(false);
